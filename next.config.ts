373f35b8d9a4cc21c5a28c53bb134e2532206c5b
Code-X-Rahul <rr611305@gmail.com>
2024-12-23 19:04:50 +0530

    next config and action updated

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,8 @@ const nextConfig: NextConfig = {
 
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  output: "export",
+  images: { unoptimized: true },
 };
 
 export default withMDX(nextConfig);
