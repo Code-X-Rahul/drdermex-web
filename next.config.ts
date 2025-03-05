2a6f35c3b07d544b35773652510f1267b7373250
Rishabh <rishabh.mh1@gmail.com>
2025-03-05 17:29:22 +0530

    fix: remove static export from next config

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,6 @@ const nextConfig: NextConfig = {
 
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-  output: "export",
   images: { unoptimized: true },
 };
 
