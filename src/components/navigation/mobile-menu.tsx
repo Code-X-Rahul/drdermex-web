8a6ec1b87246a9d3726ad1bffbc81fdf605ce9cb
Rishabh <rishabh.mh1@gmail.com>
2025-03-10 03:04:23 +0530

    fix mobile menu on navigation change auto close

--- src/components/navigation/mobile-menu.tsx
+++ src/components/navigation/mobile-menu.tsx
@@ -1,14 +1,21 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Dialog, DialogPanel } from "@headlessui/react";
 import Image from "next/image";
 import Link from "next/link";
 import { HiBars4, HiXMark } from "react-icons/hi2";
 import { Button } from "../ui/button";
+import { usePathname } from "next/navigation";
 
 const MobileMenu = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState<boolean>(false);
+
+  const pathname = usePathname();
+
+  useEffect(() => {
+    setMobileMenuOpen(false);
+  }, [pathname]);
 
   return (
     <div className='flex lg:hidden'>
@@ -25,7 +32,9 @@ const MobileMenu = () => {
         <div className='fixed inset-0 z-10' />
         <DialogPanel className='fixed inset-y-0 right-0 z-10 w-full bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10'>
           <div className='flex items-center justify-between'>
-            <Image src={"/Logo.png"} alt='DrDermex logo' width={100} height={10} />
+            <Link href='/'>
+              <Image src={"/Logo.png"} alt='DrDermex logo' width={100} height={10} />
+            </Link>
             <button
               type='button'
               onClick={() => setMobileMenuOpen(false)}
